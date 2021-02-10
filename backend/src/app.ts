02f80af57dfcf1acce86286642f8042b5beb4eff
Kamil Drzastwa <kamil@codeaddict.io>
2021-02-10 02:00:05 +0100

    FIXING PUT

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,6 @@ import User from './models/User';
 import {database} from './database/database';
 
 const cors = require('cors');
-
 const bodyParser = require('body-parser');
 
 
@@ -13,86 +12,136 @@ module.exports = class App {
     }
 
     async start() {
+        const PORT = 8000;
+
         try {
+            const app = express();
+            app.use(cors({origin: '*'}));
+            app.use(bodyParser.urlencoded({
+                extended: true
+            }));
+
+            app.use(bodyParser.json());
             await database.testConnection();
+
 
             const models = {
                 User: User.init(database.getSequelizeInstance())
             }
 
+            //password md5 crypting
             models.User.beforeCreate((async (user) => {
                 const password = user.getDataValue('passwordMd5');
                 const encryptedPassword = await md5(password);
                 user.setDataValue('passwordMd5', encryptedPassword);
-            }))
-
-            const app = express();
-            const PORT = 8000;
-
-            app.use(cors({origin: '*'}));
-            app.use(bodyParser.urlencoded({
-                extended: true
             }));
-            app.use(bodyParser.json());
 
             //CREATE - POST
             app.post('/user', async (req, res) => {
                 const {user} = req.body;
                 if (user) {
-                    console.log('[+] Adding new user', user);
-                    const createdModel = await models.User.create(user);
-                    return res.send(createdModel);
+                    models.User.create(user)
+                        .then(createdModel => {
+                            return res.send(createdModel);
+                        })
+                        .catch(error => {
+                            return res.send(`ADDING USER FAILED`).code(304);
+                        })
 
                 } else {
-                    return res.senxd({
-                        type: "Error",
-                    })
+                    return res.send(`ADDING USER FAILED`).code(304);
                 }
 
             });
 
             //READ - GET
             app.get('/users', async (req, res) => {
-                const response = await models.User.findAll({
+                models.User.findAll({
                     where: {
                         isDeleted: false
                     }
+                }).then((users) => {
+                    return res.send(users);
+                }).catch(error => {
+                    return res.send(`FETCHING USERS FAILED`).code(304);
                 });
-                return res.send(response);
             });
 
-            app.get('/user:id', async (req, res) => {
-
+            app.get('/user/:id', async (req, res) => {
+                const {id} = req.params;
+                models.User.findAll({
+                    where: {
+                        id
+                    }
+                }).then((users) => {
+                    return res.send(users);
+                }).catch(error => {
+                    return res.send(`FETCHING USER FAILED`).code(304);
+                });
             });
 
 
             //UPDATE - PUT
-            /*app.put('/users/:userId', (req, res) => {
-                return res.send(
-                    `PUT HTTP method on user/${req.params.userId} resource`,
-                );
-            });*/
+            app.put('/user/:id', (req, res) => {
+                const {id} = req.params;
+                const {user} = req.body;
+
+                console.log('id on update', id)
+                console.log('req body on update', req.body)
+                if (user) {
+                    models.User.findAll({
+                        where: {
+                            id
+                        }
+                    })
+                        .then(foundUser => {
+                            console.log('found user', foundUser)
+                            foundUser[0].update({...user}).then(updatedUser => {
+                                return res.send(updatedUser);
+
+                            }).catch(error => {
+                                return res.send(`UPDATING USER FAILED`).code(304);
+                            })
+                        })
+                        .catch(error => {
+                            return res.send(`UPDATING USER FAILED`).code(304);
+                        })
+
+                } else {
+                    return res.send(`UPDATING USER FAILED - DATA NOT PROVIDED`).code(304);
+                }
+
+            });
 
             //DELETE - DELETE
-
             app.delete('/user/:id', async (req, res) => {
                 console.log('app delete called!', req.params.id);
                 const {id} = req.params;
 
-                const user = await models.User.findAll({
+                await models.User.findAll({
                     where: {
                         id
                     }
+                }).then((user) => {
+                    console.log('deleting user!', user[0].getDataValue('surname'), user[0].getDataValue('isDeleted'));
+                    user[0].update({
+                        isDeleted: true
+                    })
+                        .then(() => {
+                            return res.send(
+                                `DELETE SUCCEEDED`,
+                            );
+                        })
+                        .catch(error => {
+                            return res.send(
+                                `DELETE FAILED`,
+                            ).code(304);
+                        })
+                }).catch(error => {
+                    return res.send(
+                        `DELETE FAILED`,
+                    ).code(304);
                 })
-                console.log('deleting user!', user[0].getDataValue('surname'), user[0].getDataValue('isDeleted'));
-
-                await user[0].update({
-                    isDeleted: true
-                });
-
-                return res.send(
-                    `DELETE HTTP method on user/${id} resource`,
-                );
             })
 
             app.listen(PORT, () => {
--- frontend/src/backendQueries/queries.ts
+++ frontend/src/backendQueries/queries.ts
@@ -8,3 +8,13 @@ export const getAllUsers = () => axios.get(SERVER_ADDRESS + '/users')
 export const addUser = (user: User) => axios.post(SERVER_ADDRESS + '/user', {user});
 
 export const deleteUser = (id: string) => axios.delete(SERVER_ADDRESS + '/user/' + id);
+
+export const updateUser = (id: string, updates: User) => axios.put(SERVER_ADDRESS + '/user/' + id, {updates});
+
+updateUser("3c529bf3-b4e5-41b1-8c5a-9af703d012b4", {
+    name: "Elon Musk",
+    surname: "musk",
+    login: "login",
+    isDeleted: false,
+    passwordMd5: "lalllala"
+});
